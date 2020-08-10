4b00e35092d47e9a04a7019c3f803c5b09630ec6
Chenqiao Yin <31758167+MrWolffy@users.noreply.github.com>
2020-08-10 19:53:45 +0800

    create tutorial blog

---
+++ blogproject/blog/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    name = 'blog'
+    verbose_name = '博客'
+
+
+class CommentsConfig(AppConfig):
+    name = 'comments'
+    verbose_name = '评论'
